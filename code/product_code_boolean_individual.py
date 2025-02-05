1f3e48056b14115fab13094445932eae264ee1f1
unknown <wenbo.ma@foxmail.com>
2025-02-04 19:08:05 -0500

    c = 0.3

--- code/product_code_boolean_individual.py
+++ code/product_code_boolean_individual.py
@@ -8,7 +8,7 @@ import itertools
 import re
 import gurobipy as gp
 from gurobipy import GRB
-import gurobipy_pandas as gppd
+# import gurobipy_pandas as gppd
 
 ########## define helper functions
 def return_omega_index(node, q_thresh):
